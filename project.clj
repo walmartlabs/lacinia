2de62b1ec090953b99bd047b2488bc1888a06940
Howard M. Lewis Ship <hlship@walmartlabs.com>
2017-03-15 16:48:37 -0700

    Setup proper test reporting for Circle CI

--- project.clj
+++ project.clj
@@ -2,12 +2,14 @@
   :description "A GraphQL server implementation in Clojure"
   :license {:name "Apache, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :plugins [[lein-codox "0.10.2"]]
+  :plugins [[lein-codox "0.10.2"]
+            [test2junit "1.2.5"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojure-future-spec "1.9.0-alpha14"]
                  [clj-antlr "0.2.4"]
                  [org.flatland/ordered "1.5.4"]]
   :profiles {:dev {:dependencies [[criterium "0.4.4"]
                                   [org.clojure/data.json "0.2.6"]]}}
+  :test2junit-output-dir ~(or (System/getenv "CIRCLE_TEST_REPORTS") "target/test2junit")
   :codox {:source-uri "https://github.com/walmartlabs/lacinia/blob/master/{filepath}#L{line}"
           :metadata   {:doc/format :markdown}})
