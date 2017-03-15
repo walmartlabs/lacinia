dd28d99569a829a08344143f657bc3131f485eda
Howard M. Lewis Ship <hlship@walmartlabs.com>
2017-03-15 15:37:04 -0700

    Prepare for v0.13.0

---
+++ CHANGES.md
@@ -0,0 +1,4 @@
+## 0.13.0 -- 15 Mar 2017
+
+Lucky 13 is our first publicly available version of Lacinia.
+It is still alpha and still subject to change, however.
--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.walmartlabs/lacinia "0.13.0"
-  :description "A GraphQL server implementation"
+  :description "A GraphQL server implementation in Clojure"
   :license {:name "Apache, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :plugins [[lein-codox "0.10.2"]]
