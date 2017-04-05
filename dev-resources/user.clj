3fc60bbdf8e2ec09ca4575afd193889a4e1ce39f
Howard M. Lewis Ship <hlship@walmartlabs.com>
2017-04-05 11:58:08 -0700

    Revert unwanted changes
    These were disabled early when they prevented the REPL from starting up

--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -1,11 +1,11 @@
 (ns user
-  #_(:require [criterium.core :as c]
+  (:require [criterium.core :as c]
             [com.walmartlabs.lacinia :as g]
             [org.example.schema :refer [star-wars-schema]]))
 
-#_(def schema (star-wars-schema))
+(def schema (star-wars-schema))
 
-#_(defn q
+(defn q
   ([query]
     (q query nil))
   ([query vars]
