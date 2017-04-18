0752095798745ac1a4001990656bcf3a8b1c548d
Howard M. Lewis Ship <hlship@walmartlabs.com>
2017-04-18 09:00:15 -0700

    Move the create-charts script down into perf, make a few improvements

--- perf/create-charts.R
+++ perf/create-charts.R
@@ -3,16 +3,18 @@
 # Execute with `Rscript create-charts.R`
 # Reads perf/benchmarks.csv and generates two PNG charts into perf
 
-# Read the benchmarks in as 'b'
-
 library("ggplot2")
 
 uniquefactor <- function(v) {
   factor(v, ordered=TRUE, levels=unique(v))
 }
 
+# Read the benchmarks in as 'b'
+
 # Need to do some work here to convert the commit and kind columns into ordered factors.
-b <- read.csv("perf/benchmarks.csv", stringsAsFactors=FALSE)
+# There's probably some mojo to do it all in the call to read.csv, but this works.
+
+b <- read.csv("benchmarks.csv", stringsAsFactors=FALSE)
 b$date = factor(b$date, ordered=TRUE)
 b$commit = uniquefactor(b$commit)
 b$kind = factor(b$kind, ordered=TRUE)
@@ -30,10 +32,10 @@ create_chart <- function(file_name, type_name, data) {
       theme(plot.title = element_text(size=18),
         axis.text.x=element_text(angle=45, hjust=1))
 
-  cat("Saving chart as ", file_name, "\n")
+  cat("Saving chart as ", file_name, "\n", sep="")
 
-  ggsave(file_name, parse.plot)
+  ggsave(file_name, parse.plot, width=10, height=7)
 }
 
-create_chart("perf/parse-time.png", "Parse", b$parse)
-create_chart("perf/exec-time.png", "Execution", b$exec)
+create_chart("parse-time.pdf", "Parse", b$parse)
+create_chart("exec-time.pdf", "Execution", b$exec)
