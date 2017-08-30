e5dd2a793009dd770773d2012dd124cbf7fb5873
Howard M. Lewis Ship <hlship@walmartlabs.com>
2017-08-30 10:47:19 -0700

    Update dependencies, revert change to test.check

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
             [test2junit "1.2.5"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojure-future-spec "1.9.0-alpha17"]
-                 [org.clojure/test.check "0.9.0"]
                  [clj-antlr "0.2.4"]
-                 [org.flatland/ordered "1.5.5"
+                 [org.flatland/ordered "1.5.6"
                   :exclusions [org.clojure/tools.macro]]]
   :profiles {:dev {:dependencies [[criterium "0.4.4"]
                                   [joda-time "2.9.9"]
+                                  [org.clojure/test.check "0.9.0"]
                                   [org.clojure/data.csv "0.1.4"]
                                   [org.clojure/tools.cli "0.3.5"]
                                   [org.clojure/data.json "0.2.6"]]}}
