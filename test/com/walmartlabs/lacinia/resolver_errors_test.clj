a421b65787bdbaa4315129b2586e2af01c927aa2
Steve Ashton <nothsaevets@gmail.com>
2018-03-22 09:49:46 -0600

    Add test to demonstrate error propagation problem

--- test/com/walmartlabs/lacinia/resolver_errors_test.clj
+++ test/com/walmartlabs/lacinia/resolver_errors_test.clj
@@ -1,9 +1,11 @@
 (ns com.walmartlabs.lacinia.resolver-errors-test
   "Tests for errors and exceptions inside field resolvers, and for the exception converter."
   (:require
-    [clojure.test :refer [deftest is]]
-    [com.walmartlabs.lacinia.resolve :refer [resolve-as]]
-    [com.walmartlabs.test-utils :refer [execute] :as utils])
+   [clojure.test :refer [deftest is testing]]
+   [com.walmartlabs.lacinia.resolve :refer [resolve-as]]
+   [com.walmartlabs.test-utils :refer [execute simplify] :as utils]
+   [com.walmartlabs.lacinia.schema :as schema]
+   [com.walmartlabs.lacinia :as lacinia])
   (:import (clojure.lang ExceptionInfo)))
 
 (def ^:private failure-exception (ex-info "Fail!" {:reason :testing}))
@@ -67,3 +69,47 @@
          (->> (execute default-schema "{ root { multiple_errors_field }}")
               :errors
               (sort-by :message)))))
+
+(deftest errors-are-propagated
+  (testing "errors are propagated from sub-selectors even when no data is returned"
+    (let [container-data {"empty-container" {:contents []}
+                          "full-container" {:contents [{:name "Book"}
+                                                       {:name "Picture"}]}}
+          schema (schema/compile {:objects
+                                  {:item
+                                   {:fields {:name {:type 'String}}}
+                                   :container
+                                   {:fields {:id {:type 'String}
+                                             :contents {:type '(list :item)
+                                                        :resolve (fn [ctx args v]
+                                                                   (println (:contents v))
+                                                                   (resolve-as
+                                                                    (:contents v)
+                                                                    [{:message "Some error"}]))}}}}
+                                  :queries
+                                  {:container {:type :container
+                                               :args {:id {:type 'String}}
+                                               :resolve (fn [ctx args v]
+                                                          (get container-data (:id args)))}}})
+          execute-query (fn [q]
+                          (-> (lacinia/execute schema q {} nil nil)
+                              (simplify)))]
+      (testing "when the sub-selector returns data"
+        (let [q "query foo { container(id:\"full-container\") { contents { name } } }"]
+          (is (= {:data {:container {:contents [{:name "Book"}
+                                                {:name "Picture"}]}}
+                  :errors [{:locations [{:column 43
+                                         :line 1}]
+                            :message "Some error"
+                            :query-path [:container
+                                         :contents]}]}
+                 (execute-query q)))))
+      (testing "when the sub-selector returns an empty collection"
+        (let [q "query foo { container(id:\"empty-container\") { contents { name } } }"]
+          (is (= {:data {:container {:contents []}}
+                  :errors [{:locations [{:column 43
+                                         :line 1}]
+                            :message "Some error"
+                            :query-path [:container
+                                         :contents]}]}
+                 (execute-query q))))))))
