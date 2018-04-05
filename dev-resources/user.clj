50f70b3f4c6f47845ac6348369da8c4faa2ebe07
Howard M. Lewis Ship <hlship@walmartlabs.com>
2018-04-05 14:29:28 -0700

    Tweak user.clj to have more useful requires

--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -1,3 +1,6 @@
 (ns user
-  (:require [criterium.core :as c]
-            [com.walmartlabs.lacinia :as l]))
+  (:require
+    [criterium.core :as c]
+    [clojure.java.io :as io]
+    [com.walmartlabs.lacinia.schema :as schema]
+    [com.walmartlabs.lacinia :as l]))
