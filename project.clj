ab85abb5ef10621a269c26e75bfeb607a000fb03
Howard M. Lewis Ship <hlship@walmartlabs.com>
2020-01-23 15:39:26 -0800

    Advance to 0.36.0-alpha-3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.walmartlabs/lacinia "0.36.0-alpha-2"
+(defproject com.walmartlabs/lacinia "0.36.0-alpha-3"
   :description "A GraphQL server implementation in Clojure"
   :url "https://github.com/walmartlabs/lacinia"
   :license {:name "Apache, Version 2.0"
