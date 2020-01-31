6ae9f028966753577c896c98f78c77aa3d615183
Howard M. Lewis Ship <hlship@walmartlabs.com>
2020-01-31 10:57:16 -0800

    Advance dependencies, prep for 0.36.0-alpha-4

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
-(defproject com.walmartlabs/lacinia "0.36.0-alpha-3"
+(defproject com.walmartlabs/lacinia "0.36.0-alpha-4"
   :description "A GraphQL server implementation in Clojure"
   :url "https://github.com/walmartlabs/lacinia"
   :license {:name "Apache, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :plugins [[lein-codox "0.10.3"]
             [test2junit "1.2.5"]]
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [clj-antlr "0.2.5"]
                  [org.flatland/ordered "1.5.7"
                   :exclusions [org.clojure/tools.macro]]
-                 [org.clojure/data.json "0.2.6"]]
+                 [org.clojure/data.json "0.2.7"]]
   :source-paths ["src"
                  "vendor-src"]
   :profiles {:dev {:dependencies [[criterium "0.4.5"]
-                                  [expound "0.7.2"]
-                                  [joda-time "2.10.3"]
+                                  [expound "0.8.4"]
+                                  [joda-time "2.10.5"]
                                   [com.walmartlabs/test-reporting "0.1.0"]
                                   [io.aviso/logging "0.3.2"]
                                   [io.pedestal/pedestal.log "0.5.5"]
