d06bd199e0f2536eec41a03800f11ae6968133a9
Howard Lewis Ship <h@lewisship.net>
2020-09-04 11:27:04 -0700

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,22 +1,22 @@
 (defproject com.walmartlabs/lacinia "0.37.0"
   :description "A GraphQL server implementation in Clojure"
   :url "https://github.com/walmartlabs/lacinia"
-  :license {:name "Apache, Version 2.0"
+  :license {:name "Apache, Version 2.0"'
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :plugins [[lein-codox "0.10.7"]
             [test2junit "1.2.5"]]
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [clj-antlr "0.2.5"]
+                 [clj-antlr "0.2.6"]
                  [org.flatland/ordered "1.5.9"]
                  [org.clojure/data.json "1.0.0"]]
   :source-paths ["src"]
-  :profiles {:dev {:dependencies [[criterium "0.4.5"]
+  :profiles {:dev {:dependencies [[criterium "0.4.6"]
                                   [expound "0.8.5"]
                                   [joda-time "2.10.6"]
                                   [com.walmartlabs/test-reporting "1.0.0"]
                                   [io.aviso/logging "0.3.2"]
                                   [io.pedestal/pedestal.log "0.5.5"]
-                                  [org.clojure/test.check "1.0.0"]
+                                  [org.clojure/test.check "1.1.0"]
                                   [org.clojure/data.csv "1.0.0"]
                                   [org.clojure/tools.cli "1.0.194"]]}}
   :aliases {"benchmarks" ["run" "-m" "perf"]}
