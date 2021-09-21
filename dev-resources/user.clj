11de87a40e74a949a6e4391bb5e65532b7ddda7f
Howard Lewis Ship <h@lewisship.net>
2021-09-21 14:22:53 -0700

    Add the trace namespace

--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -6,9 +6,21 @@
     [com.walmartlabs.lacinia.schema :as schema]
     [com.walmartlabs.lacinia :as l]
     [clojure.spec.alpha :as s]
+    [com.walmartlabs.lacinia.trace :refer [set-compile-trace!
+                                           set-enable-trace!]]
     [expound.alpha :as expound]))
 
 (require 'com.walmartlabs.lacinia.expound)
 
 (alter-var-root #'s/*explain-out* (constantly expound/printer))
+
+(comment
+  (do
+    (set! *warn-on-reflection* true)
+    (set-compile-trace! true)
+    (set-enable-trace! true))`
+
+  (set-enable-trace! false)
+
+  )
 
