e29c27450771aa2b68fa239082ee1c53e77ae5c6
Howard Lewis Ship <h@lewisship.net>
2021-11-02 16:17:21 -0700

    Prepare 1.1-alpha-5a

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.walmartlabs/lacinia "1.1-alpha-5"
+(defproject com.walmartlabs/lacinia "1.1-alpha-5a"
   :description "A GraphQL server implementation in Clojure"
   :url "https://github.com/walmartlabs/lacinia"
   :license {:name "Apache, Version 2.0"
